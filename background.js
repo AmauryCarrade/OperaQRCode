559bbab5b506f06cae490aecaacbdf4266c00438
J-C Bubbendorf <jc.bubbendorf@gmail.com>
2012-05-27 21:12:47 +0200

    Initial commit (v1.0)

---
+++ background.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function(){
+    var button;
+    var UIItemProperties = {
+        disabled: false,
+        title: titlePopup,
+        icon: "icons/qrcode_mini.png",
+        popup: {
+            href: "popup.html",
+            width: 300,
+            height: 300
+        }
+    }
+    button = opera.contexts.toolbar.createItem(UIItemProperties);
+    opera.contexts.toolbar.addItem(button);
+
+    opera.extension.onconnect = function(event) {
+        var thisTab = opera.extension.tabs.getFocused();
+        event.source.postMessage(thisTab.url + '%%%%%%%%' + loading);
+    }
+}, false);
